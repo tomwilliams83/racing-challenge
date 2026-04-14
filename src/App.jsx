41022ffaf56ac23d4f07868bfd791cb4917463dc
tomwilliams83 <tomwilliams77@hotmail.com>
2026-04-14 16:44:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import {
 // ─── FIREBASE ─────────────────────────────────────────────────────────────────
 const firebaseConfig = {
   apiKey:      import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId:   import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId:       import.meta.env.VITE_FIREBASE_APP_ID,
